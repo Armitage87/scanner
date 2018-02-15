d4b15ec558ac3b95f7b3d1103ac31fdabeb6e53d
Harvey Dumancic <Harvey.Dumancic@fleetmatics.com>
2018-02-15 08:55:08 +0000

    Initial commit

---
+++ transform_experiment2.py
@@ -0,0 +1,72 @@
+from transform import four_point_transform
+import imutils
+from skimage.filters import threshold_adaptive, threshold_local
+import numpy as np
+import argparse
+import cv2
+import pytesseract
+import os
+from PIL import Image
+
+
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe'
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to the image to be scanned")
+args = vars(ap.parse_args())
+
+
+image = cv2.imread(args["image"])
+ratio = image.shape[0] / 500.0
+orig = image.copy()
+image = imutils.resize(image, height=500)
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.GaussianBlur(gray, (5, 5), 0)
+edged = cv2.Canny(gray, 75, 200)
+
+
+print("Edge Detection")
+#cv2.imshow("Image", image)
+#cv2.imshow("Edged", edged)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+
+
+(image, cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
+
+for c in cnts:
+    peri = cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+
+    if len(approx) == 4:
+        screenCnt = approx
+        break
+
+
+print("Finding contours of paper")
+cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
+cv2.imshow("Outline", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
+
+
+warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
+warped = threshold_local(warped, 5, offset = 10)
+warped = warped.astype("uint8") * 255
+filename = "{}.png".format(os.getpid())
+cv2.imwrite(filename, warped)
+
+
+print("Applying perspective transform")
+cv2.imshow("Original", imutils.resize(orig, height = 650))
+cv2.imshow("Scanned", imutils.resize(warped, height = 650))
+cv2.waitKey(0)
+
+
+text = pytesseract.image_to_string(Image.open(filename))
+os.remove(filename)
+print(text)
